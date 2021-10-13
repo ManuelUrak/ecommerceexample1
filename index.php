e4368cad54965284199756da7e833b65abc40d17
unknown <manuel.urak88@gmail.com>
2021-10-13 19:01:20 +0200

    added top and main menu

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
 
@@ -35,6 +36,79 @@
                         <a href="checkout.php">Login</a>
                     </li>
                 </ul>
+            </div>
+        </div>
+    </div>
+
+    <!-- Main Menu -->
+
+    <div id="navbar" class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand home">
+                    <img src="images/ecom-store-logo.png" alt="M-Dev-Store Logo" class="hidden-xs">
+                    <img src="images/ecom-store-logo-mobile.png" alt="M-Dev-Store Logo" class="visible-xs">
+                </a>
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <i class="fa fa-align-justify"></i>
+                </button>
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                    <span class="sr-only">Toggle Search</span>
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse" id="navigation">
+                <div class="padding-nav">
+                    <ul class="nav navbar-nav left">
+                        <li class="active">
+                            <a href="index.php">Home</a>
+                        </li>
+                        <li>
+                            <a href="shop.php">Shop</a>
+                        </li>
+                        <li>
+                            <a href="checkout.php">My Account</a>
+                        </li>
+                        <li>
+                            <a href="cart.php">Cart</a>
+                        </li>
+                        <li>
+                            <a href="contact.php">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!--Shopping Cart -->
+
+                <a href="cart.php" class="btn navbar-btn btn-primary right">
+                    <i class="fa fa-shopping-cart"></i>
+                    <span>4 Items In Your Cart</span>
+                </a>
+
+                <!-- Search Toggle On Mobile View -->
+
+                <div class="navbar-collapse collapse right">
+                    <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search">
+                        <span class="sr-only">Toggle Search</span>
+                        <i class="fa fa-search"></i> 
+                    </button>  
+                </div>
+
+                <!-- Search Toggle On Desktop -->
+
+                <div class="collapse clearfix" id="search">
+                    <form action="results.php" method="get" class="navbar-form">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="user_query" placeholder="Search" required>
+                            <span class="input-group-btn">
+                                <button class="btn btn-primary" type="submit" value="Search" name="search">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
