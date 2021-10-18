1cdac0bcadddb964ffee397ce350cfa969456ea4
unknown <manuel.urak88@gmail.com>
2021-10-18 18:44:35 +0200

    added shop site

---
+++ includes/sidebar.php
@@ -0,0 +1,28 @@
+<!-- Sidebar -->
+
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">Categories</h3>
+    </div>
+    <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked category-menu">
+            <li><a href="#">Shirts</a></li>
+            <li><a href="#">Pants</a></li>
+            <li><a href="#">Dresses</a></li>
+            <li><a href="#">Jackets</a></li>
+            <li><a href="#">Shoes</a></li>
+        </ul>
+    </div>
+</div>
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">Fashion For</h3>
+    </div>
+    <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked category-menu">
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Woman</a></li>
+            <li><a href="#">Kids</a></li>
+        </ul>
+    </div>
+</div>
