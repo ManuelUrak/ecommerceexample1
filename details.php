9c46d090f549024ca4e0b6ef186c485c499b0350
unknown <manuel.urak88@gmail.com>
2021-10-25 19:33:25 +0200

    made details page dynamic

--- details.php
+++ details.php
@@ -1,7 +1,28 @@
 <?php 
 
+include("includes/db.php");
 include("includes/header.php");
 include("includes/navigation.php");
+
+if(isset($_GET['pro_id'])){
+    $product_id = $_GET['pro_id'];
+    $get_product = "SELECT * FROM products WHERE product_id=$product_id";
+    $run_product = mysqli_query($con, $get_product);
+    $row_product = mysqli_fetch_array($run_product);
+    $p_cat_id = $row_product['p_cat_id'];
+    $pro_title = $row_product['product_title'];
+    $pro_price = $row_product['product_price'];
+    $pro_desc = $row_product['product_desc'];
+    $pro_img1 = $row_product['product_img1'];
+    $pro_img2 = $row_product['product_img2'];
+    $pro_img3 = $row_product['product_img3'];
+
+    $get_p_cat = "SELECT * FROM product_categories WHERE p_cat_id=$p_cat_id";
+    $run_p_cat = mysqli_query($con, $get_p_cat);
+    $row_p_cat = mysqli_fetch_array($run_p_cat);
+    $p_cat_title = $row_p_cat['p_cat_title'];
+
+}
 
 ?>
 
@@ -12,7 +33,13 @@ include("includes/navigation.php");
                 <li>
                     <a href="index.php">Home</a>
                 </li>
-                <li>Shop</li>
+                <li>
+                    <a href="shop.php">Shop</a>
+                </li>
+                <li>
+                    <a href="shop.php?p_cat=<?php echo "$p_cat_id"; ?>"><?php echo "$p_cat_title"; ?></a>
+                </li>
+                <li><?php echo "$pro_title"; ?></li>
             </ul>
         </div>
 
@@ -44,17 +71,17 @@ include("includes/navigation.php");
                             <div class="carousel-inner">
                                 <div class="item active">
                                     <center>
-                                        <img class="image-responsive" src="admin_area/product_images/Product-3a.jpg" alt="Image">
+                                        <img class="image-responsive" src="admin_area/product_images/<?php echo "$pro_img1" ?>" alt="Image">
                                     </center>
                                 </div>
                                 <div class="item">
                                     <center>
-                                        <img class="image-responsive" src="admin_area/product_images/Product-3b.jpg" alt="Image">
+                                        <img class="image-responsive" src="admin_area/product_images/<?php echo "$pro_img2" ?>" alt="Image">
                                     </center>
                                 </div>
                                 <div class="item">
                                     <center>
-                                        <img class="image-responsive" src="admin_area/product_images/Product-3c.jpg" alt="Image">
+                                        <img class="image-responsive" src="admin_area/product_images/<?php echo "$pro_img3" ?>" alt="Image">
                                     </center>
                                 </div>
                             </div>
@@ -74,7 +101,7 @@ include("includes/navigation.php");
                     <!-- Product Information -->
 
                     <div class="box">
-                        <h1 class="center">Polo Shirt</h1>
+                        <h1 class="center"><?php echo "$pro_title"; ?></h1>
                         <form action="" class="form-horizontal" method="post">
                             <div class="form-group">
                                 <label for="" class="col-md-5 control-label">Product Quantity</label>
@@ -100,24 +127,24 @@ include("includes/navigation.php");
                                     </select>
                                 </div>
                             </div>
-                            <p class="price">$30</p>
+                            <p class="price">$ <?php echo "$pro_price"; ?></p>
                             <p class="text-center buttons"><button class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Add To Cart</button></p>
                         </form>
                     </div>
                     <div class="row" id="thumbs">
                         <div class="col-xs-4">
                             <a href="#" data-target="#myCarousel" data-slide-to="0" class="thumb">
-                                <img src="admin_area/product_images/Product-3a.jpg" alt="Image" class="img-responsive">
+                                <img src="admin_area/product_images/<?php echo "$pro_img1" ?>" alt="Image" class="img-responsive">
                             </a>
                         </div>
                         <div class="col-xs-4">
                             <a href="#" data-target="#myCarousel" data-slide-to="1" class="thumb">
-                                <img src="admin_area/product_images/Product-3b.jpg" alt="Image" class="img-responsive">
+                                <img src="admin_area/product_images/<?php echo "$pro_img2" ?>" alt="Image" class="img-responsive">
                             </a>
                         </div>
                         <div class="col-xs-4">
                             <a href="#" data-target="#myCarousel" data-slide-to="2" class="thumb">
-                                <img src="admin_area/product_images/Product-3c.jpg" alt="Image" class="img-responsive">
+                                <img src="admin_area/product_images/<?php echo "$pro_img3" ?>" alt="Image" class="img-responsive">
                             </a>
                         </div>
                     </div>
@@ -128,7 +155,7 @@ include("includes/navigation.php");
 
             <div class="box" id="details">
                 <h4>Product Details</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident hic est itaque minima illo, possimus excepturi assumenda mollitia voluptas ullam eum! Molestias rerum architecto deleniti, quis distinctio repudiandae. Explicabo, corrupti?</p>
+                <p><?php echo "$pro_desc"; ?></p>
                 <hr>
             </div>
 
